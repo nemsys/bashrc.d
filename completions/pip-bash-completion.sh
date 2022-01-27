f949568c28b1b6926a8bad1645deb0014fcc6cdf
nemsys <iva.e.popova@gmail.com>
2022-01-28 00:40:23 +0200

    use new pip completions

--- completions/pip-bash-completion.sh
+++ completions/pip-bash-completion.sh
@@ -29,3 +29,14 @@ complete -o default -F _pip pip2
 complete -o default -F _pip pip3
 
 # /* vim: set filetype=sh ts=8: */
+
+# pip bash completion start
+_pip_completion()
+{
+    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   PIP_AUTO_COMPLETE=1 $1 2>/dev/null ) )
+}
+complete -o default -F _pip_completion pip
+# pip bash completion end
+
