b5b8b476e76bb1b70a6cb929fc76c4e8fd5632d7
nemsys <iva.e.popova@gmail.com>
2025-11-07 19:04:34 +0200

    fix

---
+++ .private/aliases.sh
@@ -0,0 +1,9 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Place to define all custom aliases                                 #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+alias my_projects="cd ${HOME}/projects/"
+
+### git:
+# sort by comotterdate (ASC) and show:
+alias git_branch_sort_committerdate="git branch --sort=committerdate --format='%(committerdate:short) %(refname:short)'"
+
